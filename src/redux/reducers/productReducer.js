a8b891182b6f5b59f4bd17366f76629ff899c9f3
anikdev2016 <anikdev2016@gmail.com>
2023-01-04 21:54:18 +0600

    created the switch case conditions in product reducer function

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,7 +1,20 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTypes";
+
 // initial state
 export const initialState = { a: "hello" };
 
 // reducer function
 export const productReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case ADD_TO_CART: {
+      // something will happen if the case is add to cart
+      return {};
+    }
+    case REMOVE_FROM_CART: {
+      // something will happen if the case is remove from the cart
+      return {};
+    }
+    default:
+      return state;
+  }
 };
